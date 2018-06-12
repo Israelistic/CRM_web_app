bb5d06fde84179dc28550c64a6c6d73b6880faba
hlerman <haguyle@gmail.com>
2018-06-12 18:35:20 -0400

    1.added the option to view each profile on its own view. 2.added not found page if the contact does no exist. 3.added CSS style sheet "layout" to work with every view.

--- crm.rb
+++ crm.rb
@@ -8,14 +8,23 @@ end
 get "/contacts" do
   @contacts = Contact.all
   erb :contacts
-
 end
+
 get "/about" do
   erb :about
 end
 
 get "/search" do
   erb :search
+end
+get "/show_contact/:id" do
+ @contacts = Contact.find_by({id: params[:id].to_i})
+    if @contacts != nil
+    erb :show_contacts
+    else
+    raise Sinatra::NotFound
+
+    end
 end
 after do
   ActiveRecord::Base.connection.close
