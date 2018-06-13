362816a8896e918c2a21ab95609802d945512954
hlerman <haguyle@gmail.com>
2018-06-13 15:20:39 -0400

    chenged edit from a link to a button and added delete method.

--- crm.rb
+++ crm.rb
@@ -9,8 +9,63 @@ get "/contacts" do
   @contacts = Contact.all
   erb :contacts
 end
+
 get "/contacts/new" do
   erb :new
+end
+
+get "/show_contact/:id" do
+ @contacts = Contact.find_by({id: params[:id].to_i})
+    if @contacts != nil
+    erb :show_contacts
+    else
+    raise Sinatra::NotFound
+    end
+end
+
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contacts = Contact.find_by(id: params[:id].to_i)
+  if @contacts
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 get "/about" do
@@ -20,14 +75,10 @@ end
 get "/search" do
   erb :search
 end
-get "/show_contact/:id" do
- @contacts = Contact.find_by({id: params[:id].to_i})
-    if @contacts != nil
-    erb :show_contacts
-    else
-    raise Sinatra::NotFound
-    end
-end
+
+
+
+
 after do
   ActiveRecord::Base.connection.close
 end
